2fe50d52a3db82a91f56fc88d6ddcea6faca341a
aramyunis <aaram2361@gmail.com>
2021-12-20 09:20:49 +0300

    setup socketIo

--- src/app.js
+++ src/app.js
@@ -3,18 +3,34 @@ const authRoute = require("./routers/auth");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const http = require("http");
+const { Server } = require("socket.io");
+
+//socket
+const server = http.createServer(app);
+const io = new Server(server);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
 require("dotenv").config({
   path: "../.dev.env",
 });
+
+//api
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
 
 // parse application/json
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 app.use("/api", authRoute);
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log(`Server is running on http://localhost:${3000}`);
 });
